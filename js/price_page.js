11786f8e714a4513fd26a20e00ba2e26dbab1c96
Prodip <p.k.sorkar007@gmail.com>
2022-11-24 13:42:22 +0600

    Price calculators are done

---
+++ js/price_page.js
@@ -0,0 +1,42 @@
+function calculatePrice(formId) {
+  let form = document.getElementById(formId);
+  let total_price = 0;
+  let numbers = {
+    quantity: null,
+    rate: null,
+    service: null,
+    time: null,
+  };
+
+  for (let i = 0; i <= form.length; i++) {
+    if (form[i]) {
+      if (form[i].type != "submit") {
+        if (form[i].value != 0) {
+          for (let j = 0; j <= Object.keys(numbers).length; j++) {
+            if (Object.keys(numbers)[j] == form[i].name) {
+              let x = Object.keys(numbers)[j];
+              numbers[x] = form[i].value;
+            }
+          }
+        }
+      }
+    }
+  }
+  let service_rate = Number(numbers.rate) + Number(numbers.service)
+  total_price = numbers.quantity * service_rate;
+  total_price = total_price + (total_price*(numbers.time/100))
+  total_price = total_price.toFixed(2);
+  if (form) {
+    if (form.children) {
+      for (let k = 0; k <= form.children.length; k++) {
+        if (form.children[k]) {
+          if (form.children[k].children) {
+            if (form.children[k].children.totalOutput) {
+              form.children[k].children.totalOutput.innerHTML = "$" + total_price;
+            }
+          }
+        }
+      }
+    }
+  }
+}
